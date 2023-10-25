1838e4642e3c3e63310aea4502739acaaacfe47f
metncheese <muhammad2304ft.2021@student.uny.ac.id>
2023-10-25 14:27:18 +0700

    Commit pertama

--- app/src/main/java/app/raihan/loginassignment/MainActivity.kt
+++ app/src/main/java/app/raihan/loginassignment/MainActivity.kt
@@ -1,11 +1,26 @@
 package app.raihan.loginassignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textViewMoveActivity: View = findViewById(R.id.text_move_activity)
+        textViewMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.text_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, RegisterSignUp::class.java)
+                startActivity(moveIntent)
+            }
+        }
     }
 }
